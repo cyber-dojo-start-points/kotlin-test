922a7c13e67937fb32ec193ecc80a4ea39429067
OvisMaximus <OvisMaximus@users.noreply.github.com>
2022-02-18 13:14:58 +0100

    Drop ESC sequences to keep test result readable
    There is no way we are aware of to prevent kotlintest writing ESC sequences to switch output color in the terminal.
    The used terminal does not alter font collor in accordance to ESC sequences.
    To keep the output tidy and readable, drop all ESC sequences for color manipulation of the used font.

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -3,9 +3,7 @@
 CLASSES=.:`ls /usr/share/kotlin/kotlinc/lib/*.jar | tr '\n' ':'`
 CLASSES=`ls /kotlin/*.jar | tr '\n' ':'`${CLASSES}
 kotlinc *.kt -include-runtime -cp $CLASSES
+EXECUTE_TEST="java -cp $CLASSES io.kotlintest.runner.console.LauncherKt --writer basic --spec hiker.HikerTest"
 if [ $? -eq 0 ]; then
-  java -cp $CLASSES \
-    io.kotlintest.runner.console.LauncherKt \
-    --writer basic \
-    --spec hiker.HikerTest  # [X]
+   $EXECUTE_TEST | sed 's/\x1b\[[0-9;]*m//g'
 fi
